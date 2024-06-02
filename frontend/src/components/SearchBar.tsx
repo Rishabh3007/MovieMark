73ff17201d1a567d9f8071c1e23d45adcb674f4e
Rishabh Jain <jainrishabh3007@gmail.com>
2024-06-03 01:35:18 +0530

    deployment test

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,52 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const SearchBar: React.FC = () => {
-//     const [searchTerm, setSearchTerm] = useState('');
-//     const [movies, setMovies] = useState([]);
-
-//     useEffect(() => {
-//         const fetchMovies = async () => {
-//             try {
-//                 const response = await axios.get(`http://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=${process.env.REACT_APP_API_KEY}`);
-//                 console.log(response.data.Search);
-//                 setMovies(response.data.Search);
-//             } catch (error) {
-//                 console.error('Error fetching movies:', error);
-//             }
-//         };
-
-//         fetchMovies();
-//     }, [searchTerm]);
-
-//     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//         setSearchTerm(event.target.value);
-//     };
-
-//     return (
-//         <div>
-//             <input
-//                 type="text"
-//                 placeholder="Search movies..."
-//                 value={searchTerm}
-//                 onChange={handleSearchChange}
-//                 className="border border-gray-300 rounded px-4 py-2"
-//             />
-//             <div className=''>
-//                 {movies?.length > 0 && movies.map((movie: any) => (
-//                     <div key={movie.imdbID}>
-//                         <h2 className='text-white'>{movie.Title}</h2>
-//                         <img src={movie.Poster} alt={movie.Title} />
-//                     </div>
-//                 ))}
-//             </div>
-            
-//         </div>
-//     );
-// };
-
-// export default SearchBar;
-
 import React, { useState, useEffect, useCallback} from 'react';
 import axios from 'axios';
 import debounce from 'lodash.debounce';
@@ -99,6 +50,12 @@ const SearchBar: React.FC = () => {
         setTimeout(() => setIsFocused(false), 100); // Delay to allow click events on suggestions
     };
 
+    const handleMovieClick = (imdbID: string) => {
+        console.log(imdbID);
+        setSearchTerm('');
+        setIsFocused(false);
+    };
+
     return (
         <div className="bg-transparent text-white">
             <div className="max-w-lg mx-auto relative">
@@ -115,7 +72,9 @@ const SearchBar: React.FC = () => {
                     <div className="absolute left-0 right-0 mt-2 bg-gray-800 rounded shadow-lg z-50">
                         <ul className="space-y-1 p-1">
                             {movies.slice(0, 7).map((movie: any) => (
-                                <li key={movie.imdbID} className="flex items-center p-1 hover:bg-gray-700 rounded">
+                                <li key={movie.imdbID} className="flex items-center p-1 hover:bg-gray-700 rounded"
+                                    onClick={() => handleMovieClick(movie.imdbID)}
+                                >
                                     <img src={movie.Poster} alt={movie.Title} className="w-10 h-16 object-cover rounded mr-4" />
                                     <div>
                                         <h2 className="text-md">{movie.Title}</h2>
